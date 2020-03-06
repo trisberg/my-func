e2554b6bb6cf2af6525a0cc5dc3b45092f02612a
Thomas Risberg <trisberg@pivotal.io>
2020-03-06 16:30:27 -0500

    added POJO and conversion from JSON

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class HelloWorld {
+
+    private String msg;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorld [msg=" + msg + "]";
+    }
+}
--- src/main/java/com/example/myfunc/MyFuncApplication.java
+++ src/main/java/com/example/myfunc/MyFuncApplication.java
@@ -4,7 +4,10 @@ import java.util.Map;
 import java.util.TreeMap;
 import java.util.function.Function;
 
+import com.example.HelloWorld;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,13 +21,20 @@ public class MyFuncApplication {
 
 	Logger log = LoggerFactory.getLogger(MyFuncApplication.class);
 
+	ObjectMapper objectMapper = new ObjectMapper();
+
 	@Bean
 	public Function<Message<JsonNode>, String> hello() {
 		return m -> {
 			Map<String, String> headers = extractCloudEventsHeaders(m.getHeaders());
 			log.info("HEADERS: " + headers);
 			log.info("PAYLOAD: " + m.getPayload());
-			return m.getPayload().toString();
+			HelloWorld pojo = convertPayload(m.getPayload());
+			if (pojo != null) {
+				return pojo.toString();
+			} else {
+				return "Unable to parse: " + m.getPayload();
+			}
 		};
 	}
 
@@ -36,6 +46,15 @@ public class MyFuncApplication {
 			}
 		}
 		return headers;
+	}
+
+	private HelloWorld convertPayload(JsonNode payload) {
+		try {
+			return objectMapper.treeToValue(payload, HelloWorld.class);
+		} catch (JsonProcessingException e) {
+			log.error(e.getMessage());
+			return null;
+		}
 	}
 
 	public static void main(String[] args) {
